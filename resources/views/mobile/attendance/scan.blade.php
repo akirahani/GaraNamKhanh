2d99bb223fe1233e14b58029c1a83c36918e9efb
huu <tranthhienlong@gmail.com>
2021-07-15 10:12:03 +0700

    1

--- resources/views/mobile/attendance/scan.blade.php
+++ resources/views/mobile/attendance/scan.blade.php
@@ -5,52 +5,71 @@
     <input type="text" id="session" value="{!! $item->session !!}">    
 @endforeach
 
-
+<script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/10.0.3/highlight.min.js"></script>
 <script src="{{ asset('assets2/js/html5-qrcode.min.js') }}"></script>
 <script src="{{ asset('assets2/js/sweetalert2.all.min.js') }}"></script>
 <script src="{{ asset('assets2/js/jquery-latest.pack.js') }}"></script>
 
 <script>
-
-    const html5QrCode = new Html5Qrcode("reader");
-    
-    const qrCodeSuccessCallback = (decodedText, decodedResult) => {
-        
-        document.getElementById("results").value = decodedText;
-        
-        var send = $('#results').val();
-        var session = $('#session').val();
-        
-        if(send == session){
-          $.ajax({
-            type: "POST",
-            url: "{!! route('member.attendcane.store') !!}",
-            data:{
-                "_token": "{{ csrf_token() }}",
-                "id":  {!! $member->id  !!},
-              
-            },
-            success: function(){
-                Swal.fire({
-                  position: 'center',
-                  icon: 'success',
-                  title: 'Your work has been saved',
-                  showConfirmButton: false,
-                  timer: 1500
-                });
-                setTimeout(function() {
-                window.location.replace("{!! route('member.home') !!}");}
-                , 3000);
-                }
-          });
+    function docReady(fn) {
+        // see if DOM is already available
+        if (document.readyState === "complete" || document.readyState === "interactive") {
+            // call on next available tick
+            setTimeout(fn, 1);
+        } else {
+            document.addEventListener("DOMContentLoaded", fn);
         }
-      html5QrCode.stop();
-      
-    };
+    }
+    /** Ugly function to write the results to a table dynamically. */
+    
+    docReady(function() {
+            let html5QrcodeScanner = new Html5QrcodeScanner(
+            "reader", 
+            { 
+                fps: 10,
+                qrbox: 250,
+                // Important notice: this is experimental feature, use it at your
+                // own risk. See documentation in
+                // mebjas@/html5-qrcode/src/experimental-features.ts
+                experimentalFeatures: {
+                    useBarCodeDetectorIfSupported: true
+                }
+            });
+            hljs.initHighlightingOnLoad();
+            var lastMessage;
+            var codeId = 0;
+            function onScanSuccess(decodedText, decodedResult) {
+            document.getElementById("results").value = decodedText;
+        
+                var send = $('#results').val();
+                var session = $('#session').val();
 
-    const config = { fps: 10, qrbox: 500, aspectRatio: 1.58888,experimentalFeatures: {
-                useBarCodeDetectorIfSupported: true
-            } };
-    html5QrCode.start({ facingMode: "environment" }, config, qrCodeSuccessCallback);
+                if(send == session){
+                  $.ajax({
+                    type: "POST",
+                    url: "{!! route('member.attendcane.store') !!}",
+                    data:{
+                        "_token": "{{ csrf_token() }}",
+                        "id":  {!! $member->id  !!},
 
+                    },
+                    success: function(){
+                        Swal.fire({
+                          position: 'center',
+                          icon: 'success',
+                          title: 'Your work has been saved',
+                          showConfirmButton: false,
+                          timer: 1500
+                        });
+                        setTimeout(function() {
+                        window.location.replace("{!! route('member.home') !!}");}
+                        , 3000);
+                        }
+                  });
+                }
+              html5QrcodeScanner.stop();
+            }
+            
+            html5QrcodeScanner.render(onScanSuccess);
+    });
 </script>
