33c0f6b50eb1c2d4cec33d9af21960321eefadfc
huu <tranthhienlong@gmail.com>
2021-07-15 10:14:39 +0700

    1

--- resources/views/mobile/attendance/scan.blade.php
+++ resources/views/mobile/attendance/scan.blade.php
@@ -3,9 +3,10 @@
         <div id="reader" style="width:100%"></div>
         <input id="results" type="text" >
     </body>
-    <script src="/html5-qrcode.min.js"></script>
+    <script src="assets2/js/html5-qrcode.min.js"></script>
     <script>
-          const html5QrCode = new Html5Qrcode("reader");
+        
+        const html5QrCode = new Html5Qrcode("reader");
     
         const qrCodeSuccessCallback = (decodedText, decodedResult) => {
             
