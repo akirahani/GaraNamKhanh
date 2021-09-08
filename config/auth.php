5db2d0fcc068fd0119c2da4da0a2b6daec3c2113
huu <tranthhienlong@gmail.com>
2021-09-08 09:38:53 +0700

    1

--- config/auth.php
+++ config/auth.php
@@ -43,7 +43,7 @@ return [
 
         'api' => [
             'driver' => 'passport',
-            'provider' => 'members',
+            'provider' => 'users',
             // 'hash' => false,
         ],
 
